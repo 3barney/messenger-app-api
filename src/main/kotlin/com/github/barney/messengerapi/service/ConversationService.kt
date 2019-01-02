403a66b74b7fdfa9e9fd0d7062828464639c5036
barney <brnbsjm@gmail.com>
2019-01-02 11:57:50 +0300

    converation service and associated implementation, just missing Exception handler class

--- src/main/kotlin/com/github/barney/messengerapi/service/ConversationService.kt
+++ src/main/kotlin/com/github/barney/messengerapi/service/ConversationService.kt
@@ -5,5 +5,10 @@ import com.github.barney.messengerapi.models.User
 
 interface ConversationService {
 
-
+    fun createConversation(userA: User, userB: User): Conversation
+    fun conversationExists(userA: User, userB: User): Boolean
+    fun getConversation(userA: User, userB: User): Conversation?
+    fun retrieveThread(conversationId: Long): Conversation
+    fun listUserConversations(userId: Long): List<Conversation>
+    fun nameSecondParty(conversation: Conversation, userId: Long): String
 }
